db58044b6993509806d4163323b27e671d36e6e1
gareth5james <gareth5james@yahoo.co.uk>
2024-11-03 18:36:33 +0000

    first draft

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { handlePasswordCreation } from "../Utilities/handlePasswordCreation";
+import { copyText } from "../Utilities/copyText";
 
 function Main() {
     const [length, setLength] = useState(16);
@@ -8,28 +9,54 @@ function Main() {
     const [numbers, setNumbers] = useState(false);
     const [symbols, setSymbols] = useState(false);
     const [password, setPassword] = useState("");
+    const [start, setStart] = useState(false)
 
     return <main className="main-section">
+        <fieldset>
         <label className="label" htmlFor="setLength">Desired Length</label>
         <input className="input" type="number" id="setLength" name="setLength" value={length} min="5" max="50" onChange={(event) => setLength(event.target.value)}></input>
+        </fieldset>
         <br/>
+        <fieldset>
         <label className="label" htmlFor="lowercase">Lowercase?</label>
         <input className="input" type="checkbox" id="lowercase" name="lowercase" value={lowercase} onChange={(event) => {setLowercase(lowercase === false ? true : false)}}></input>
+        </fieldset>
         <br/>
+        <fieldset>
         <label className="label" htmlFor="uppercase">Uppercase?</label>
         <input className="input" type="checkbox" id="uppercase" name="uppercase" value={uppercase} onChange={(event) => {setUppercase(uppercase === false ? true : false)}}></input>
+        </fieldset>
         <br/>
+        <fieldset>
         <label className="label" htmlFor="numbers">Numbers?</label>
         <input className="input" type="checkbox" id="numbers" name="numbers" value={numbers} onChange={(event) => {setNumbers(numbers === false ? true : false)}}></input>
+        </fieldset>
         <br/>
+        <fieldset>
         <label className="label" htmlFor="symbols">Symbols?</label>
         <input className="input" type="checkbox" id="symbols" name="symbols" value={symbols} onChange={(event) => {setSymbols(symbols === false ? true : false)}}></input>
+        </fieldset>
         <br/>
+        <fieldset>
+        <label className="label" htmlFor="start">Start with a letter?</label>
+        <input className="input" type="checkbox" id="start" name="start" value={start} onChange={(event) => {setStart(start === false ? true : false)}}></input>
+        </fieldset>
+        <br/>
+        <fieldset className="buttonFields">
         <button className="button" onClick={(event) => {
             event.preventDefault();
-            setPassword(handlePasswordCreation(length, lowercase, uppercase, numbers, symbols))
+            setPassword(handlePasswordCreation(length, lowercase, uppercase, numbers, symbols, start))
         }}>Get Passwords</button>
+        </fieldset>
+        <fieldset>
         <div className="pwDiv" id="pwDiv">{password}</div>
+        </fieldset>
+        <fieldset className="buttonFields">
+        <button className="button" onClick={(event) => {
+            event.preventDefault();
+            copyText(password);
+        }}>Copy to Clipboard</button>
+        </fieldset>
     </main>
 }
 
--- src/Utilities/handlePasswordCreation.js
+++ src/Utilities/handlePasswordCreation.js
@@ -3,7 +3,37 @@ exports.handlePasswordCreation = (
   lowercase,
   uppercase,
   numbers,
-  symbols
+  symbols,
+  start
 ) => {
-  return "vmoeirovmvoeigjeoighnovenivioenvoe";
+  if (!lowercase && !uppercase && !numbers && !symbols && !start)
+    return "Please select some options";
+
+  let password = "";
+
+  let pool = "";
+
+  let text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  if (lowercase) pool += "abcdefghijklmnopqrstuvwxyz";
+
+  if (uppercase) pool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  if (numbers) pool += "0123456789";
+
+  if (symbols) pool += "!;#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  while (password.length < length) {
+    let char;
+
+    password.length === 0 && start
+      ? (char = text[Math.floor(Math.random() * text.length)])
+      : (char = pool[Math.floor(Math.random() * pool.length)]);
+
+    password += char;
+  }
+
+  console.log(password.length);
+
+  return password;
 };
